ca4b78700d7c3ef161dfd6caa9759204d42f1c4a
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-10-18 10:11:42 +0200

    Add project files.

---
+++ Inlämningsuppgift 4.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uppgift_4._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i<=30; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i=200;  i>=180; i--)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 1000; i<=1400; i += 50)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
